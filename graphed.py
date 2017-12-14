91933c210bebf7159878c9a26261d06ccb4226d0
d0iasm <doiasami1219@gmail.com>
2017-12-15 08:59:12 +0900

    Add graphviz test file

---
+++ graphed.py
@@ -0,0 +1,17 @@
+import graphviz
+
+graph = graphviz.Digraph(format='png')
+graph.attr('node', shape='circle')
+
+n = 15
+
+for i in range(n):
+    graph.node(str(i), str(i))
+
+for i in range(n):
+    if(i - 1) // 2 >= 0:
+        graph.edge(str((i-1)//2), str(i))
+
+
+print(graph)
+graph.render('graph')
