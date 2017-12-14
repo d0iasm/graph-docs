e4a5a6530a9e3c558e3a2c0f72616e842ae83a6b
d0iasm <doiasami1219@gmail.com>
2017-12-14 12:44:33 +0900

    Add an official slide

--- parser.py
+++ parser.py
@@ -1,33 +1,3 @@
-# from pyknp import Juman
-# import sys
-# import codecs
-# sys.stdin = codecs.getreader('utf_8')(sys.stdin)
-# sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
-
-# Use Juman in subprocess mode
-# juman = Juman()
-# result = juman.analysis(u"これはペンです。")
-# print(','.join(mrph.midasi for mrph in result))
-
-# for mrph in result.mrph_list():
-# print("見出し:%s, 読み:%s, 原形:%s, 品詞:%s, 品詞細分類:%s, 活用型:%s, 活用形:%s, 意味情報:%s, 代表表記:%s" \
-# % (mrph.midasi, mrph.yomi, mrph.genkei, mrph.hinsi, mrph.bunrui, mrph.katuyou1, mrph.katuyou2, mrph.imis, mrph.repname))
-
-# Use Juman in server mode
-# juman = Juman(server='localhost', port=12345)
-
-# from pyknp import Juman
-# import sys
-# import codecs
-# sys.stdin = codecs.getreader('utf_8')(sys.stdin)
-# sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
-# JUMAN++をsubprocessモードで使用
-# juman = Juman()
-# result = juman.analysis(u"ケーキを食べる")
-# for mrph in result.mrph_list():
-# print(u"見出し:{0}".format(mrph.midasi))
-
-
 import pyknp
 
 
@@ -51,8 +21,20 @@ def select_dependency_structure(line):
     return tuples
 
 
+def test_juman(line):
+    juman = pyknp.Juman()
+    result = juman.analysis(line)
+    print(','.join(mrph.midasi for mrph in result))
+
+    for mrph in result.mrph_list():
+        print("見出し:%s, 読み:%s, 原形:%s, 品詞:%s, 品詞細分類:%s, 活用型:%s, 活用形:%s, 意味情報:%s, 代表表記:%s" \
+    % (mrph.midasi, mrph.yomi, mrph.genkei, mrph.hinsi, mrph.bunrui, mrph.katuyou1, mrph.katuyou2, mrph.imis, mrph.repname))
+
+
+
 if __name__ == '__main__' :
     line = '太郎は花子が読んでいる本を次郎に渡した'
     tuples = select_dependency_structure(line)
+    test_juman(line)
     for t in tuples:
         print(t[0] + ' => ' + t[1])
