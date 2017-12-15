c2273289068a39cd9aa4b1713b7e96cef24c602a
d0iasm <doiasami1219@gmail.com>
2017-12-15 12:45:31 +0900

    Remove unused methods

--- parser.py
+++ parser.py
@@ -1,12 +1,6 @@
 import pyknp
 
 
-def select_normalization_representative_notation(fstring):
-    begin = fstring.find('正規化代表表記:')
-    end = fstring.find('/', begin + 1)
-    return fstring[begin + len('正規化代表表記:'): end]
-
-
 def find_original_word(bunsetsu):
     """
     @param bunsetsu pyknp.knp.bunsetsu Class
@@ -15,48 +9,23 @@ def find_original_word(bunsetsu):
     return bunsetsu.mrph_list()[0].genkei
 
 
-def select_dependency_structure(line):
+def find_parent_child(line):
     knp = pyknp.KNP()
     result = knp.parse(line)
     bnst_list = result.bnst_list()
- 
-    for bnst in bnst_list:
-        print(find_original_word(bnst))
-
-    bnst_dic = dict((x.bnst_id, x) for x in bnst_list)
+    bnst_dict = dict((x.bnst_id, x) for x in bnst_list)
 
     tuples = []
     for bnst in bnst_list:
         if bnst.parent_id != -1:
             tuples.append((find_original_word(bnst),
-                                find_original_word(bnst_dic[bnst.parent_id])))
-            # tuples.append((select_normalization_representative_notation(
-                # bnst.fstring), select_normalization_representative_notation(bnst_dic[bnst.parent_id].fstring)))
+                           find_original_word(bnst_dict[bnst.parent_id])))
 
     return tuples
 
 
-def test_juman(line):
-    juman = pyknp.Juman()
-    result = juman.analysis(line)
-    print(','.join(mrph.midasi for mrph in result))
-
-    for mrph in result.mrph_list():
-        print('見出し:%s, 読み:%s, 原形:%s, 品詞:%s, 品詞細分類:%s, 活用型:%s, 活用形:%s, 意味情報:%s, 代表表記:%s'
-              % (mrph.midasi, mrph.yomi, mrph.genkei, mrph.hinsi, mrph.bunrui, mrph.katuyou1, mrph.katuyou2, mrph.imis, mrph.repname))
-
-
-def simple(line):
-    knp = pyknp.KNP()
-    print(vars(knp.parse(line)))
-
-
-
 if __name__ == '__main__':
     line = '太郎は花子が読んでいる本を次郎に渡した'
-    tuples = select_dependency_structure(line)
-    # test_juman(line)
+    tuples = find_parent_child(line)
     for t in tuples:
         print(t[0] + ' => ' + t[1])
-
-    # simple(line)
