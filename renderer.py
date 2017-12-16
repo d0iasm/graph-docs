80f841f12c4c27eb6bed2ae57bfbae7cfcd0b2b6
d0iasm <doiasami1219@gmail.com>
2017-12-16 15:11:25 +0900

    Add pydoc in methods

--- renderer.py
+++ renderer.py
@@ -11,27 +11,54 @@ class Renderer(object):
         self.dot.attr('node', shape='circle')
 
     def add_edge(self, child, parent):
+        """
+        :param string child: a child name of a node.
+        :param string parent: a parent name of a node.
+        """
         self.dot.edge(child, parent)
 
     def add_edges(self, line):
+        """
+        :param string line: a natural language text for parsing.
+        """
         for child, parent in parser.find_parent_child(line):
             self.dot.edge(child, parent)
 
     def add_node(self, name, label=None):
+        """
+        :param string name: a node name.
+        :param string label: a visable node name. optional.
+        """
         self.dot.node(name, label)
 
     def add_nodes(self, line):
+        """
+        :param string line: a natural language text for parsing.
+        """
         for node in parser.find_nodes(line):
             self.dot.node(str(node), str(node))
 
     def copy(self, src, dest):
+        """
+        :param string src: a file name in a source.
+        :param string dest: a file name in a destination.
+        """
         subprocess.call(['cp', src, dest])
 
     def render_from_dot(self, src, img):
+        """
+        :param string src: a dot file name in a source.
+        :param string img: an image file name in a destination.
+        """
         # TODO: Remove view=True
         graphviz.Source(open(src, 'r').read(), format='png').render(img, view=True)
 
     def merge(self, old, new, out):
+        """
+        :param string old: a old dot file name.
+        :param string new: a new dot file name.
+        :param string out: a merged dot file name.
+        """
         # cmd = 'gvpack -u old.dot new.dot | sed \'s/_gv[0-9]\+//g\' | dot -Tpng -o result.png'
         # res = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
 
@@ -49,6 +76,9 @@ class Renderer(object):
             ps.wait()
 
     def save(self, name):
+        """
+        :param string name: a file name.
+        """
         self.dot.save(filename=name)
 
     def update_shape(self, shape):
