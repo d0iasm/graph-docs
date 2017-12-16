b3d5d05d480e3179862f07e297d5de61608b3bdd
d0iasm <doiasami1219@gmail.com>
2017-12-16 19:58:15 +0900

    Add a mention file

---
+++ plugins/mention.py
@@ -0,0 +1,16 @@
+from slackbot.bot import listen_to
+
+
+count = 0
+text = ''
+
+
+@listen_to('(.*)')
+def listen_func(message, content):
+    global count, text
+    count += 1
+    text += content
+    if count >= 2:
+        message.reply('Create a graph from the following text \n```' + text + '```')
+        count = 0
+        text = 0
