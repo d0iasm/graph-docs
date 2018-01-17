e82154133b519765c36583bef69a76036942cd9f
d0iasm <doiasami1219@gmail.com>
2018-01-17 23:15:17 +0900

    Update a feature of removing swapwords

--- plugins/parser.py
+++ plugins/parser.py
@@ -18,6 +18,8 @@ class Parser(object):
     def find_nodes(self):
         nodes = []
         for word in self.words:
+            if word in self.swapwords: continue
+                
             nodes.append((word, self.__weighting(word)))
         return nodes
 
@@ -28,9 +30,11 @@ class Parser(object):
     
         tuples = []
         for bnst in bnst_list:
+            original = self.__find_original_word(bnst)
+            if original in self.swapwords: continue
+                
             if bnst.parent_id != -1:
-                tuples.append((self.__find_original_word(bnst),
-                               self.__find_original_word(bnst_dict[bnst.parent_id])))
+                tuples.append((original, self.__find_original_word(bnst_dict[bnst.parent_id])))
 
         return tuples
 
@@ -38,6 +42,7 @@ class Parser(object):
     def set(self, text):
         self.line = self.__remove_marks(text)
         self.words = self.__find_words()
+        self.swapwords = self.__get_swapwords()
         self.counters = Counter(self.words)
 
     
@@ -67,7 +72,7 @@ class Parser(object):
         return bnst_list
     
 
-    def __get_swapwords(self, line):
+    def __get_swapwords(self):
         slothlib_path = 'http://svn.sourceforge.jp/svnroot/slothlib/CSharp/Version1/SlothLib/NLP/Filter/StopWord/word/Japanese.txt'
         slothlib_file = urllib.request.urlopen(slothlib_path)
         slothlib_stopwords = [l.decode("utf-8").strip() for l in slothlib_file]
@@ -91,15 +96,7 @@ class Parser(object):
 
     
 if __name__ == '__main__':
-    line = """2017年11月に設立されたメルペイ。メルカリが金融関連の新規事業を行うために立ち上げた子会社だ。同社の代表取締役には元グリーCFOの青柳直樹氏が就任し、役員には元WebPayのCTOでLINE Pay事業を経験した曾川景介氏らが名を連ねるなど注目を集めている。
-今まで事業の詳細については明らかになっていなかったが、年内にも仮想通貨交換業の登録申請をして、メルカリ内の決済手段としてビットコインを含む仮想通貨に対応していくようだ。
-もう一つはコンパイルせず、検索時にパターンを設定する方法です。
-この場合、検索パターンを使いまわすことがない場合はこちらを使うとよいでしょう。
-
-2017年11月に設立されたメルペイ。メルカリが金融関連の新規事業を行うために立ち上げた子会社だ。同社の代表取締役には元グリーCFOの青柳直樹氏が就任し、役員には元WebPayのCTOでLINE Pay事業を経験した曾川景介氏らが名を連ねるなど注目を集めている。
-今まで事業の詳細については明らかになっていなかったが、年内にも仮想通貨交換業の登録申請をして、メルカリ内の決済手段としてビットコインを含む仮想通貨に対応していくようだ。
-もう一つはコンパイルせず、検索時にパターンを設定する方法です。
-この場合、検索パターンを使いまわすことがない場合はこちらを使うとよいでしょう。"""
+    line = """今回の勝負は負けそうだ。あそこに犬が一匹いる。"""
     p = Parser()
     p.set(line)
     tuples = p.find_parent_child()
