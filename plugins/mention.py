50a11650f76164c29cc50f6864c2ffc60076deba
d0iasm <doiasami1219@gmail.com>
2018-01-30 12:07:25 +0900

    Update the plugins module to PLUGINS

--- plugins/mention.py
+++ plugins/mention.py
@@ -14,8 +14,8 @@ text = ''
 def help(message):
     message.reply("""
 You can ask me one of the following questions by mentioning such as ``@graphy``:
-``リセット`` or ``reset``: You can delete all past text.
-``ヘルプ`` or ``help``: You can know how to use this bot. this message will be sent.
+`リセット` or `reset`: You can delete all past text.
+`ヘルプ` or `help`: You can know how to use this bot. this message will be sent.
 
 This bot listen all text and create an image from it automatically if you invite this bot and do not mention.
 You can remove this bot whenever you want to.
--- slackbot_settings.py
+++ slackbot_settings.py
@@ -5,7 +5,7 @@ API_TOKEN = os.environ['SLACKBOT_API_TOKEN']
 
 PLUGINS = [
     'slackbot.plugins',
-    'graphy.plugins'
+    'plugins'
     ]
 
-DEFAULT_REPLY = "You can ask me for help by ``@graphy help`` or ``@graphy ヘルプ``"
+DEFAULT_REPLY = "You can ask me for help by `@graphy help` or `@graphy ヘルプ`"
